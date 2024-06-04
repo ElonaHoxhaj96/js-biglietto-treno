8eae459018ae7f1c432ec17f260d688bf3aaed5b
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-04 15:54:10 +0200

    Setting the Flowchart in JS

---
+++ myscript.js
@@ -0,0 +1,18 @@
+// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero. Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio secondo queste regole: -il prezzo del biglietto è definito in base ai km (0.21 € al km), va applicato uno sconto del 20% per i minorenni,va applicato uno sconto del 40% per gli over 65;
+
+//Ask the users: how many km their have to travel ?;
+
+
+// Aske the users their ages!;
+
+// Knowing the price per km, calculate the price by €/km;
+
+
+// If the user have les then 18 eyar(exluded), applicate a discount of 20%;
+
+
+// If the user have more then 65 eyar(included), applicate a discount of 40%;
+
+// Show the price to the users;
+
+
