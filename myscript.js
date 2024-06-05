3bc77825490828c9964dff7485b7189d7fc01f51
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-05 15:36:37 +0200

    Final price

--- myscript.js
+++ myscript.js
@@ -2,32 +2,35 @@
 
 //Ask the users: how many km their have to travel ?;
 let km = prompt("Enter the kilometers you have to travel");
+document.getElementById ('km-travel').innerHTML = 'Quanti chilometri devi percorrere? '+km+'km';
 
 // Aske the users their ages!;
 let ageUser = parseInt(prompt("Enter your age from 0 to 99"));
-let minorenne = (ageUser < 18);
-let over65 = (ageUser > 65 ) ;
+// let minorenne = (ageUser < 18);
+//let over65 = (ageUser > 65 ) ;
+document.getElementById ('human-age').innerHTML = 'Quanti anni hai? '+ ageUser;
 
 // Knowing the price per km, calculate the price by €/km;
 const europerkmorario = 0.21;
 
 // normal price of tickets;
-let price = km * 0.21;
+let price = (km * 0.21).toFixed(2);
+document.getElementById ('price-ticket').innerHTML = 'Prezzo del biglietto: '+ price+'€';
 
 console.log(ageUser, price);
 
 // discount 
+let discountminorenni;
+let discountover65;
 if ( ageUser < 18){
-    price =  (price *20/100 )
+    discountminorenni = price - ((price * 20) /100).toFixed(2);
     
 } else if ( ageUser > 65 ){
-    price = (price *40/100)
+    discountover65 = price -((price * 40) /100).toFixed(2);
 
 } else {
     price
 }
-
-
 // Show the price to the users;
-
-
+document.getElementById('discount20').innerHTML ='Essendo minorenne, hai diritto ad uno sconto del 20%, il prezzo del tuo biglietto è dunque: '+ (price - discountminorenni).toFixed(2) +'€';
+document.getElementById('discount40').innerHTML = 'Essendo over 65, hai diritto ad uno sconto del 20%, il prezzo del tuo biglietto è dunque: '+ (price - discountover65).toFixed(2)+'€';
